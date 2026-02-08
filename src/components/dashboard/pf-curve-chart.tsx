bc13565a43b3fd36f9b3ea2599869795a98cf9a6
Andre Ferreira <and.ferreeira@gmail.com>
2026-02-08 20:18:54 +0000

    o grafico esta todo cortadoo

--- src/components/dashboard/pf-curve-chart.tsx
+++ src/components/dashboard/pf-curve-chart.tsx
@@ -39,7 +39,7 @@ const PFCurveSVG = ({ health, beta, distribution, curveStyle, t }: { health: num
 
   return (
     <div className="relative w-full aspect-[2/1] max-w-lg mx-auto">
-      <svg viewBox="0 0 100 50" className="w-full h-auto" preserveAspectRatio="xMidYMin meet">
+      <svg viewBox="-10 0 115 55" className="w-full h-auto" preserveAspectRatio="xMidYMin meet">
         <defs>
           <marker id="arrow" viewBox="0 0 10 10" refX="5" refY="5" markerWidth="3" markerHeight="3" orient="auto-start-reverse">
             <path d="M 0 0 L 10 5 L 0 10 z" fill="hsl(var(--border))" />
